b806735f8bb29fc81c4d5c072ec02e37880104e8
bogdanvolynets <vbohdan5@gmail.com>
2021-04-07 17:57:32 +0300

    added webpack configurations, added autoprefixer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles.scss';
+
 const Player = (sign) => {
     return { sign };
 }
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+            MiniCssExtractPlugin.loader, 
+            "css-loader",
+            "postcss-loader" 
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+  ]
+};
